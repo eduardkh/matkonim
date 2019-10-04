4de43427e79a1eebc484ec61243974d8c7db4aa6
Eduard Khiaev <eddietheone1984@gmail.com>
2019-10-04 22:52:14 +0300

    INSTALLED_APPS, Model,register to Admin

--- matkonim/admin.py
+++ matkonim/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Matkon
 
-# Register your models here.
+class MatkonModelAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'sections', 'updated']
+    list_display_links = ['updated']
+    list_filter = ['updated', 'timestamp']
+
+    class Meta:
+        model = Matkon
+
+
+admin.site.register(Matkon, MatkonModelAdmin)
--- matkonim/models.py
+++ matkonim/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Matkon(models.Model):
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User, default=1, null=True,
+                               on_delete=models.SET_NULL)
+    title = models.CharField(max_length=120)
+    sections = models.CharField(max_length=120)
+    image = models.ImageField(null=True, blank=True, upload_to='uploads/')
+    prologue = models.TextField(null=True, blank=True)
+    prep_time = models.CharField(max_length=120, null=True, blank=True)
+    cook_time = models.CharField(max_length=120, null=True, blank=True)
+    ingredients = models.TextField(null=True, blank=True)
+    Instructions = models.TextField(null=True, blank=True)
+    remarks = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(
+        auto_now=False, auto_now_add=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
